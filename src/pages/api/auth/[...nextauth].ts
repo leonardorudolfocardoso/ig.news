4808e3f4854ec643b9f9ad7c65b77ab67ccec673
Leonardo C <leonardorudolfocardoso@gmail.com>
2021-08-24 23:07:45 -0300

    save user to faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
+import { fauna } from '../../../services/fauna';
+import { query as q } from 'faunadb';
+
 export default NextAuth({
   providers: [
     Providers.GitHub({
@@ -8,5 +11,24 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
       scope: 'read:user'
     })
-  ]
+  ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        );
+  
+        return true;
+      } catch {
+        return false;
+      }
+
+    }
+  }
 });
