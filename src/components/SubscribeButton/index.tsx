2bc9b535c7b4cfba179a97f33c20c972978b6fc9
Leonardo C <leonardorudolfocardoso@gmail.com>
2021-08-30 15:20:09 -0300

    subscribe route

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { signIn, useSession } from 'next-auth/client'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -5,10 +6,22 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const session = useSession();
+
+  async function handleSubscribeClick() {
+    if (!session) {
+      await signIn('github');
+      return;
+    } 
+
+    
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribeClick}
     >
       Subscribe now
     </button>
